702f9f6e9f0f97ee2881969c26193a5772391cc4
Sean Bruce <Seanbr01@noa.nintendo.com>
2020-01-09 13:03:01 -0800

    week 1 docs + first part of HW

---
+++ Week01/ISIT324Homework1/ISIT324Homework1/FindLast.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISIT324Homework1
+{
+    public class FindLast
+    {
+        /**
+         *Find last index of element
+         *
+         *@param x array to search
+         *@param y value to look for
+         *@return last index of y in x; -1 if absent
+         *@throw NullPointerException if x is null
+         */
+
+        public static int findLast(int[] x, int y)
+        {
+            for (int i = x.Length - 1; i > -1; i--)
+            {
+                if (x[i] == y)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        // test: x = [2, 3, 5]; y = 2; Expected = 0
+    }
+}
---
+++ Week01/ISIT324Homework1/ISIT324Homework1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ISIT324Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[] { 2, 3, 5 };
+            int y = 2;
+
+            Console.WriteLine("test: x = [2, 3, 5]; y = 2; Expected = 0");
+            Console.WriteLine($"Results: {FindLast.findLast(x, y).ToString()}");
+
+            y = 3;
+            Console.WriteLine("test: x = [2, 3, 5]; y = 3; Expected = 1");
+            Console.WriteLine($"Results: {FindLast.findLast(x, y).ToString()}");
+
+            y = 5;
+            Console.WriteLine("test: x = [2, 3, 5]; y = 5; Expected = 2");
+            Console.WriteLine($"Results: {FindLast.findLast(x, y).ToString()}");
+
+            y = 999;
+            Console.WriteLine("test: x = [2, 3, 5]; y = 999; Expected = -1");
+            Console.WriteLine($"Results: {FindLast.findLast(x, y).ToString()}");
+
+            Console.ReadLine();
+        }
+    }
+}
