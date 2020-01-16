c0ae30bb64130f37a068779db694ecd0f57b8752
Sean Bruce <Seanbr01@noa.nintendo.com>
2020-01-15 18:57:38 -0800

    corrected week 1 HW

---
+++ Week02/HW01/Program_ArrayList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayListUnion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 2, 3 };
+            int[] array2 = { 2, 3, 4 };
+
+            foreach (int num in MergeIntArray(array1, array2))
+            {
+                Console.WriteLine(num.ToString());
+            }
+
+            Console.ReadLine();
+        }
+
+        public static int[] MergeIntArray(int[] array1, int[] array2)
+        {
+            return array1.Union(array2).Cast<int>().ToArray();
+        }
+    }
+}
