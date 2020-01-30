ccc4df9cbe29ca27f8422411dc188b2d7b125e28
Sean Bruce <seanbruce@hotmail.com>
2020-01-29 17:01:17 -0800

    in class work

---
+++ Week05/324_Inclass_012920/324_Inclass_012920/YardageCalculation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NFLMath
+{
+    public class YardageCalculation
+    {
+        public static int FieldGoalDistance(string end, int lineOfScrimage)
+        {
+            const string OwnEnd = "OWN";
+            const string OppEnd = "OPP";
+            const int MidField = 50;
+            const int KickAdjustment = 17;
+
+            const int MaxLineOfScrimage = 40;
+            int startingDistanceFromGoal;
+            if (end == OwnEnd && lineOfScrimage < MaxLineOfScrimage)
+            {
+                throw new ArgumentException("Filed Goal is too long.");
+            }
+
+            if (end == OwnEnd)
+            {
+                startingDistanceFromGoal = MidField + (MidField - lineOfScrimage);
+            }
+            else if (end == OppEnd)
+            {
+                startingDistanceFromGoal = lineOfScrimage;
+            }
+            else
+            {
+                startingDistanceFromGoal = lineOfScrimage;
+            }
+
+            return startingDistanceFromGoal + KickAdjustment;
+        }
+    }
+}
---
+++ Week05/324_Inclass_012920/UnitTestProject1/FieldGoalDistance_Should.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SUT = NFLMath.YardageCalculation;
+using FluentAssertions;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class FieldGoalDistance_Should
+    {
+        [DataTestMethod]
+        [DataRow("OPP", 30, 47)]
+        public void ReturnsCorrectDistanceWhenInputsAreValid(string end, int lineOfScrimage, int expectedDistance)
+        {
+            //Arrange
+            int fieldGoalDistance;
+
+            //Act
+            fieldGoalDistance = SUT.FieldGoalDistance(end, lineOfScrimage);
+
+            //Assert
+            expectedDistance.Should().Be(fieldGoalDistance);
+        }
+    }
+}
