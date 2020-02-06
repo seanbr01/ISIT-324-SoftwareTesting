677968086d128d22bb883cc518bf2f186015dd89
Sean Bruce <seanbruce@hotmail.com>
2020-02-05 16:39:19 -0800

    HW Fixes, inclass

---
+++ Week04/HW3.01Excercise/AnvilTotalTests/CalcPricePerAnvil_Should.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SUT = AnvilTotalPriceCalcExcercise;
+
+namespace AnvilTotalTests
+{
+    [TestClass]
+    public class CalcPricePerAnvil_Should
+    {
+        [TestMethod]
+        public void CalcPricePerAnvil_Return0_WhenPriceBelow0()
+        {
+            //Arrange
+            var quantity = 10;
+            var regPrice = -999.99;
+
+            //Act
+            var results = SUT.PriceCalc.CalcPricePerAnvil(quantity, regPrice);
+
+            //Assert
+            Assert.AreEqual(0, results);
+        }
+
+        [TestMethod]
+        public void CalcPricePerAnvil_Return0_WhenQuantityBelow0()
+        {
+            //Arrange
+            var quantity = -10;
+            var regPrice = 10.00;
+
+            //Act
+            var results = SUT.PriceCalc.CalcPricePerAnvil(quantity, regPrice);
+
+            //Assert
+            Assert.AreEqual(0, results);
+        }
+
+        [DataTestMethod]
+        [DataRow(0, 0)]
+        [DataRow(1, 10)]
+        [DataRow(9, 10)]
+        [DataRow(10, 9)]
+        [DataRow(19, 9)]
+        [DataRow(20, 8)]
+        [DataRow(999, 8)]
+        public void CalcPricePerAnvil_ReturnCorrectValue_WhenQuantityIsValid(int quantity, double expected)
+        {
+            //Arrange
+            var regPrice = 10.00;
+
+            //Act
+            var results = SUT.PriceCalc.CalcPricePerAnvil(quantity, regPrice);
+
+            //Assert
+            Assert.AreEqual(expected, results);
+        }
+    }
+}
---
+++ Week04/HW3.01Excercise/AnvilTotalTests/CalcShippingCostPerAnvil_Should.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SUT = AnvilTotalPriceCalcExcercise;
+
+namespace AnvilTotalTests
+{
+    [TestClass]
+    public class CalcShippingCostPerAnvil_Should
+    {
+        [DataTestMethod]
+        [DataRow(1, 10)]
+        [DataRow(2, 20)]
+        [DataRow(3, 30)]
+        public void CalcShippingCostPerAnvil_ReturnCorrectValue_WhenInputValid(int zone, double expected)
+        {
+            //Arrange
+            double results;
+
+            //Act
+            results = SUT.PriceCalc.CalcShippingCostPerAnvil(zone);
+
+            //Assert
+            Assert.AreEqual(expected, results);
+        }
+
+        [DataTestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(4)]
+        public void CalcShippingCostPerAnvil_ThrowIndexOutOfRangeException_WhenInputIsInvalid(int zone)
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.ThrowsException<IndexOutOfRangeException>(() => SUT.PriceCalc.CalcShippingCostPerAnvil(zone));
+        }
+    }
+}
