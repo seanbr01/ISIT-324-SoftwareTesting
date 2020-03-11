7e14f7c646ca0a520f86e78b4cbda267a8efd7b2
Sean Bruce <Seanbr01@noa.nintendo.com>
2020-03-11 16:53:41 -0700

    week 10 files

---
+++ Week10/Triangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW8_Triangle
+{
+    public class Triangle
+    {
+        public int SideA { get; set; }
+        public int SideB { get; set; }
+        public int SideC { get; set; }
+
+        public string AnalyzeType()
+        {
+            int a = SideA;
+            int b = SideB;
+            int c = SideC;
+
+            if (a < 1 || b < 1 || c < 1 || a > 200 || b > 200 || c > 200)
+            {
+                throw new InvalidSideException();
+            }
+
+            if ((a>=b+c) ||(b>=a+c) || (c>=a+b))
+            {
+                return "Not a triangle";
+            }
+
+            if(a == b && b == c)
+            {
+                return "Equilateral";
+            }
+            else if (a == b || b ==c || a ==c)
+            {
+                return "Isosceles";
+            }
+            else
+            {
+                return "Scalene";
+            }
+        }
+
+    }
+
+    public class InvalidSideException : Exception
+    {
+
+    }
+}
